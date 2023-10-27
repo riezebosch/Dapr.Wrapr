4152c5b256f05680ef911fda7432e399ea3b0040
Manuel Riezebosch <mriezebosch@gmail.com>
2023-10-27 11:20:00 +0200

    chore: update slow handler exmample

--- DaprDemo.Handlers.Slow/Handler.cs
+++ DaprDemo.Handlers.Slow/Handler.cs
@@ -1,28 +1,28 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
-namespace DaprDemo.Handlers.Slow
+namespace DaprDemo.Handlers.Slow;
+
+public class Handler : IHandler<int, int>
 {
-    public class Handler : IHandler<int, int>
+    private readonly ILogger _logger;
+
+    public Handler(ILogger logger) => 
+        _logger = logger;
+
+    public async Task<int> Handle(int input) => 
+        await Slow(input);
+
+    private async Task<int> Slow(int n)
     {
-        private readonly ILogger _logger;
+        _logger.LogInformation(n.ToString());
+        if (n is 0 or 1)
+            return n;
 
-        public Handler(ILogger logger) => 
-            _logger = logger;
+        var a = Slow(n - 1);
+        var b = Slow(n - 2);
 
-        public async Task<int> Handle(int input) => 
-            await Slow(input);
-
-        private async Task<int> Slow(int n)
-        {
-            _logger.LogInformation(n.ToString());
-            if (n is 0 or 1)
-                return n;
-
-            var a = Slow(n - 1);
-            var b = Slow(n - 2);
-
-            return  await a + await b;
-        }
+        return  await a + await b;
     }
 }
